f0a4ca6c409931721efcd9df48d262f91b3e7de8
mt <mt>
2014-05-17 23:18:42 +0900

    ロガー

---
+++ src/Control/Eff/Logger.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TypeOperators, FlexibleContexts, DeriveDataTypeable, DeriveFunctor #-}
+module Control.Eff.Logger
+( log
+, logDebug
+, logNotice
+, logInfo
+, logWarn
+, logError
+, runLoggerStdIO
+) where
+
+import Control.Eff ((:>), VE(..), Eff, Member, SetMember, admin, handleRelay, inj, send)
+import Control.Eff.Lift (Lift, lift)
+import Control.Monad (when)
+import Data.Typeable (Typeable)
+import Prelude hiding (log)
+
+data LogLevel = DEBUG | NOTICE | INFO | WARN | ERROR deriving (Show, Read, Eq, Enum, Ord, Typeable)
+
+data Logger s a = Logger LogLevel s a deriving (Typeable, Functor)
+
+log :: (Typeable s, Member (Logger s) r) => LogLevel -> s -> Eff r ()
+log l s = send $ \f -> inj $ Logger l s $ f ()
+
+logDebug, logNotice, logInfo, logWarn, logError :: (Typeable s, Member (Logger s) r) => s -> Eff r ()
+logDebug = log DEBUG
+logNotice = log NOTICE
+logInfo = log INFO
+logWarn = log WARN
+logError = log ERROR
+
+runLoggerStdIO :: (Show s, Typeable s, Member (Lift IO) r, SetMember Lift (Lift IO) r)
+               => LogLevel -> Eff (Logger s :> r) a -> Eff r a
+runLoggerStdIO minL = loop minL . admin
+    where loop _ (Val a) = return a
+          loop mlv (E u) = handleRelay u (loop mlv) $
+                            \(Logger lv s f) -> when (lv >= mlv) (lift $ p lv s) >> loop mlv f
+          p lv s = putStrLn $ "[" ++ show lv ++ "] " ++ show s
